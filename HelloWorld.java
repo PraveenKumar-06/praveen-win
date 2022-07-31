60c33c063a1e7ac9a127abcbae2c8511a08eecc6
praveen <praveenthurumella@gmail.com>
2022-07-31 14:39:50 +0530

    third commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hi Praveen! Welcome to jenkins on windows");
+		System.out.println("Jenkins is up and running. Build is successful");
 	}
 }
